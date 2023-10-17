218d240b2af246cf79c2a02ff5fa74865b1eab95
Barath <iambharathpadhu@gmail.com>
2023-10-17 12:12:15 +0530

    - Created the carousel and added key down handler

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Carousel } from "./Carousel";
+
+function App() {
+  return (
+    <div className="example-container">
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
